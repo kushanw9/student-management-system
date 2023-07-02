7c65d0ddd2953e3b88ee9a2679f50b2d9e23b4de
Kushan Weerarathne <kushanweerarathne14@gmail.com>
2023-07-02 13:24:24 +0530

    initialize() method implementation completed
      -created loadAllStudents() method to load students from the database.

--- src/main/java/lk/gk/kushan/sms/controller/StudentViewController.java
+++ src/main/java/lk/gk/kushan/sms/controller/StudentViewController.java
@@ -4,13 +4,7 @@ import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
@@ -21,10 +15,7 @@ import lk.gk.kushan.sms.db.DBConnection;
 import lk.gk.kushan.sms.model.Student;
 import lk.gk.kushan.sms.model.util.Gender;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.time.LocalDate;
 
 public class StudentViewController {
@@ -89,6 +80,72 @@ public class StudentViewController {
         });
 
 
+        tblStudents.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblStudents.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        tblStudents.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        tblStudents.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("address"));
+        tblStudents.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("gender"));
+        tblStudents.getColumns().get(5).setCellValueFactory(new PropertyValueFactory<>("dateOfBirth"));
+
+        loadAllStudents();
+
+
+        Platform.runLater(() -> {
+
+            root.getScene().getAccelerators().put(new KeyCodeCombination(KeyCode.N, KeyCombination.CONTROL_ANY), () -> btnNewStudent.fire());
+            root.getScene().getAccelerators().put(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_ANY), () -> btnSaveStudent.fire());
+            root.getScene().getAccelerators().put(new KeyCodeCombination(KeyCode.F1), () -> tblStudents.requestFocus());
+        });
+
+        tblStudents.getSelectionModel().selectedItemProperty().addListener((ov,previous,current)->{
+            btnDeleteStudent.setDisable(current==null);
+            if(current == null) return;
+
+            txtId.setText(current.getId() + "");
+            txtFirstName.setText(current.getFirstName());
+            txtLastName.setText(current.getLastName());
+            txtAddress.setText(current.getAddress());
+            tglGender.selectToggle(current.getGender() == Gender.MALE ? rdMale : rdFemale);
+            txtDOB.setValue(current.getDateOfBirth());
+
+        });
+
+
+    }
+
+    private void loadAllStudents() {
+        try {
+            Connection connection= DBConnection.getInstance().getConnection();
+            Statement stm = connection.createStatement();
+            ResultSet rst = stm.executeQuery("SELECT * FROM Student");
+            ObservableList<Student> studentList = tblStudents.getItems();
+
+
+            while (rst.next()) {
+                int id = rst.getInt("id");
+                String firstName = rst.getString("first_name");
+                String lastName = rst.getString("last_name");
+                String address = rst.getString("address");
+                //Gender gender=rst.getString("gender").equals("MALE")? Gender.MALE: Gender.FEMALE;
+                Gender gender=Gender.valueOf(rst.getString("gender"));
+                Date dob = rst.getDate("dob");
+
+                studentList.add(new Student(id, firstName, lastName, address, gender, dob.toLocalDate()));
+
+            }
+
+
+            studentList.forEach((Student student) -> System.out.println(student));
+            studentList.forEach(System.out::println);// converting to the method referencing
+
+            Platform.runLater(() -> btnNewStudent.fire());
+            Platform.runLater(btnNewStudent::fire);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,"Failed to load student details,try again").showAndWait();
+            Platform.exit();
+        }
     }
 
     @FXML
